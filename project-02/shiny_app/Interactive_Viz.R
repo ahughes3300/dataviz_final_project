b9dbc52dced8055f595b5f4a8fd9dec18d1c9d3a
Alex Hughes <ahughes3300@floridapoly.edu>
2023-06-23 15:53:01 -0400

    final push updated shiny app

--- project-02/shiny_app/Interactive_Viz.R
+++ project-02/shiny_app/Interactive_Viz.R
@@ -16,7 +16,7 @@ library(plotly)
 
 #marathon_csv <- read_csv("https://raw.githubusercontent.com/reisanar/datasets/master/marathon_results_2017.csv")
 #marathon500 <- head(marathon_csv, 100)
-marathon <- read_csv("data/marathon.csv")
+marathon <- read_csv("../../data/marathon.csv")
 marathon500 <- head(marathon, 300)
 colnames(marathon500)[9] ="tenk"
 colnames(marathon500)[19] ="OfficalTime"
@@ -72,7 +72,7 @@ server <- function(input, output, session) {
   observe({
     updateSelectInput(session,"Participant_Name2", choices = marathon500$Name, selected = marathon500$Name[300])
   })
-  
+  ## sorting and processing data frames
   data <- reactive({
     req(input$Participant_Name)
     req(input$Participant_Name2)
@@ -109,10 +109,6 @@ server <- function(input, output, session) {
   })
   
   output$plot500 <- renderPlot({
-    # print(data())
-    # p<-ggplot(data(), aes(x = fct_inorder(race), y=time, fill=race)) + 
-    #   geom_col()+ylim(0,10000) 
-    # p
 
     
     p<-ggplot(data(), aes(x = fct_inorder(race), y=time, fill=Name)) + 
@@ -122,26 +118,8 @@ server <- function(input, output, session) {
     p+theme_minimal()
     
   })
-  
-  # output$selected_var <- renderText({
-  #   age1<-marathon[which(marathon500$Name == input$Participant_Name), 3]
-  #   age2<-marathon[which(marathon500$Name == input$Participant_Name2), 3]
-  #   name1 <-input$Participant_Name
-  #   name2 <-input$Participant_Name2
-  #   sex1 <- marathon[which(marathon500$Name == input$Participant_Name), 4]
-  #   sex2 <- marathon[which(marathon500$Name == input$Participant_Name2), 4]
-  #   city1 <- marathon[which(marathon500$Name == input$Participant_Name), 5]
-  #   city2 <- marathon[which(marathon500$Name == input$Participant_Name2), 5]
-  #   country1 <- marathon[which(marathon500$Name == input$Participant_Name), 7]
-  #   country2<- marathon[which(marathon500$Name == input$Participant_Name2), 7]
-  #   place1 <- marathon[which(marathon500$Name == input$Participant_Name), 19]
-  #   place2 <- marathon[which(marathon500$Name == input$Participant_Name2), 19]
-  #   gender1 <- marathon[which(marathon500$Name == input$Participant_Name), 20]
-  #   gender2 <- marathon[which(marathon500$Name == input$Participant_Name2), 20]
-  #  HTML(paste0("<h5>","Name: ", name1,'<br>', "Age: ",age1,'<br>',"Sex: ",sex1,'<br>',"City: ",city1,'<br>',"Country: ",country1,'<br>',"Place: ",place1,'<br>'," Place Gender: ",gender1,
-  #              "<hr>","Name: ", name2,'<br>', "Age: ",age2,'<br>',"Sex: ",sex2,'<br>',"City: ",city2,'<br>',"Country: ",country2,'<br>',"Place: ",place2,'<br>'," Place Gender: ",gender2))
-  # })
-  
+ 
+  #selected number 1
   output$selected_var1 <- renderText({
     age1<-marathon[which(marathon500$Name == input$Participant_Name), 3]
     name1 <-input$Participant_Name
@@ -152,7 +130,7 @@ server <- function(input, output, session) {
     gender1 <- marathon[which(marathon500$Name == input$Participant_Name), 20]
     HTML(paste0("<h5>","Name: ", name1,'<br>', "Age: ",age1,'<br>',"Sex: ",sex1,'<br>',"City: ",city1,'<br>',"Country: ",country1,'<br>',"Place: ",place1,'<br>'," Place Gender: ",gender1))
   })
-    
+  #selected number 2  
   output$selected_var2 <- renderText({
     age2<-marathon[which(marathon500$Name == input$Participant_Name2), 3]
     name2 <-input$Participant_Name2
